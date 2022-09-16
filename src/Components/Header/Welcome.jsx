ec124c560a824d3aba41399c14705852d3b7055b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 21:33:40 +0100

    removed mockup header

--- src/Components/Header/Welcome.jsx
+++ src/Components/Header/Welcome.jsx
@@ -1,11 +1,22 @@
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, Button, Heading, HStack, VStack, Text, Image, Divider, Center } from '@chakra-ui/react'
 import React from 'react'
+import FoodBG from "../../Components/Images/foodbg.png"
+
 
 export default function Introsec() {
     return (
-        <Box>
-            <VStack>
-                
+        <Box bgImage={FoodBG} height='auto' minH={'30em'}>
+            <VStack p='3em' spacing='1em'>
+                <Text color='white' fontFamily='Montserrat' fontSize='1.5em' fontWeight={700} align='center'>Food App</Text>
+                <Heading textAlign='center' color='white'>
+                    Why stay hungry when <br />
+                    you can order form Bella Onojie
+                </Heading>
+                <Text color='white' fontFamily='Montserrat' fontSize='1.5em' fontWeight={500} align='center'>Download the bella onoje’s food app now on</Text>
+                <HStack>
+                    <Button bgColor='#FA4A0C' bgRepeat='no-repeat' objectFit='cover'>Playstore</Button>
+                    <Button>App Store</Button>
+                </HStack>
             </VStack>
         </Box>
     )
