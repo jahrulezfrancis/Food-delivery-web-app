96494a763eb98591752df8668db8003444f66c96
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 20:42:46 +0100

    Initial commit

---
+++ src/Components/Header/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Box, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Introsec() {
+    return (
+        <Box>
+            <VStack>
+                
+            </VStack>
+        </Box>
+    )
+}
