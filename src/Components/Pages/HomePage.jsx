d5171826e89e065e6b1189c21158058a34477bbc
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 19:51:29 +0100

    Initial commit

---
+++ src/Components/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Heading, Stack } from '@chakra-ui/react';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <Stack>
+            <Heading>Yeah you know we that we up</Heading>
+        </Stack>
+    )
+}
