925d0738bb43fde12d9f3fe88a446b158297fc12
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 20:43:40 +0100

    Added the header introduction section

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
 import Navigation from '../Header/Navigation';
+import Introsec from '../Header/Welcome';
 
 export default function HomePage() {
     return (
         <Stack>
             <Navigation />
+            <Introsec />
         </Stack>
     )
 }
