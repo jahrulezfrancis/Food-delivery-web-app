4e8a82789b776eaa75b61b359cc6324619caa95c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 20:08:21 +0100

    added the header navigation

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
+import Navigation from '../Header/Navigation';
 
 export default function HomePage() {
     return (
         <Stack>
-            <Heading>Yeah you know we that we up</Heading>
+            <Navigation />
         </Stack>
     )
 }
