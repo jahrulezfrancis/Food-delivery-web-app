f41084c1ea256dfa7948bc930110513d9d98e748
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-16 22:07:46 +0100

    completed the Mockup Section

--- src/Components/Header/AppMockUp.jsx
+++ src/Components/Header/AppMockUp.jsx
@@ -2,7 +2,29 @@ import { Box, Flex, Heading, VStack, HStack, Text, Center, Divider, Image } from
 import React from 'react';
 import MobileMockRed from "../../Components/Images/mobile-mockup-red.png"
 import MobileMockWhite from "../../Components/Images/mobile-mockup-white.png"
+import MockSec1 from "../../Components/Images/MockSec1.png"
+import MockSec2 from "../../Components/Images/MockSec2.png"
+import MockSec3 from "../../Components/Images/MockSec3.png"
 
+
+function MockupTemp(props) {
+    return (
+        <Flex direction={{ sm: 'column', md: 'column', lg: 'row' }}>
+            <Image src={props.image} alt='' />
+            <VStack justify='center' align='start' spacing='1em'>
+                <Text color='#FA4A0C'>{props.heading}</Text>
+                <Heading fontFamily='Montserrat' color='#252B42'>
+                    {props.lineone} <br />
+                    {props.linetwo}
+                </Heading>
+                <Text fontFamily='Montserrat' color='#737373'>
+                    {props.line2one} <br />
+                    {props.line2two}
+                </Text>
+            </VStack>
+        </Flex>
+    )
+}
 
 export default function AppMockUp() {
     return (
@@ -16,9 +38,26 @@ export default function AppMockUp() {
                     <Divider orientation='horizontal' size='80%' />
                 </Center>
                 <Heading>How The App Works</Heading>
+                <MockupTemp image={MockSec1} heading='Create an account' lineone='Create/Login to an existing'
+                    linetwo='account to get started' line2one='An account is created with your email' line2two='and a desired password' />
+
                 <Flex direction={{ sm: 'column', md: 'column', lg: 'row' }}>
-            
+                    <VStack justify='center' align='start' spacing='1em'>
+                        <Text color='#FA4A0C'>Explore Varieties</Text>
+                        <Heading fontFamily='Montserrat' color='#252B42'>
+                            Shop for your favorites <br />
+                            meal as e dey hot.
+                        </Heading>
+                        <Text fontFamily='Montserrat' color='#737373'>
+                            Shop for your favorite meals or drinks <br />
+                            and enjoy while doing it.
+                        </Text>
+                    </VStack>
+                    <Image src={MockSec1} alt='' />
                 </Flex>
+
+                <MockupTemp image={MockSec1} heading='Create an account' lineone='Create/Login to an existing'
+                    linetwo='account to get started' line2one='An account is created with your email' line2two='and a desired password' />
             </VStack>
         </Box>
     )
