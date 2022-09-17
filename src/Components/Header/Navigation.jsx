662488389265536ca514305566dade2c7464caa8
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-17 11:04:03 +0100

    Added mobile navigation

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,14 +1,16 @@
-import { Box, Flex, ListItem, UnorderedList, Image, HStack, Spacer, } from '@chakra-ui/react';
+import {
+    Box, Flex, ListItem, UnorderedList, Image, HStack, Spacer,
+    Menu, MenuButton, MenuList, MenuItem,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import HeaderLogo from "../../Components/Images/header-logo.png"
 import FoodBG from "../../Components/Images/foodbg.png"
-import { ClassNames } from '@emotion/react';
 
 export default function () {
 
     return (
-        <Box>
+        <Box display={{ sm: 'none', md: 'none', lg: 'block' }}>
             <UnorderedList listStyleType='none'>
                 <HStack spacing='2em' align='center' mr='2em'>
                     <NavLink to='/' >
@@ -29,6 +31,36 @@ export default function () {
                     </NavLink>
                 </HStack>
             </UnorderedList>
+        </Box>
+    )
+}
+
+
+export function MobileMenu() {
+    return (
+        <Box display={{sm: 'none', md: 'none', lg: 'block'}}>
+            <Menu>
+                <MenuButton
+                    as={IconButton}
+                    aria-label='Options'
+                    icon={<HamburgerIcon />}
+                    variant='outline'
+                />
+                <MenuList>
+                    <MenuItem icon={<AddIcon />} command='⌘T'>
+                        New Tab
+                    </MenuItem>
+                    <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
+                        New Window
+                    </MenuItem>
+                    <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
+                        Open Closed Tab
+                    </MenuItem>
+                    <MenuItem icon={<EditIcon />} command='⌘O'>
+                        Open File...
+                    </MenuItem>
+                </MenuList>
+            </Menu>
         </Box>
     )
 }
