513fa2db69ad547985f7cd39aa96328b3d7a1e00
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-17 11:38:52 +0100

    edited navigation

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import { Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
 import AppMockUp from '../Header/AppMockUp';
-import Navigation from '../Header/Navigation';
+import Navigation, { MobileMenu } from '../Header/Navigation';
 import Introsec from '../Header/Welcome';
 
 export default function HomePage() {
     return (
         <Stack>
             <Navigation />
+            <MobileMenu />
             <Introsec />
             <AppMockUp />
         </Stack>
